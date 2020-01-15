ae696396f3d1e467adf94451d5852b278f3312c8
Luke Horgan <horgan.l@husky.neu.edu>
2020-01-15 18:12:41 -0500

    this is annoying

--- h.js
+++ h.js
@@ -332,8 +332,8 @@ class UrlProcessor {
                         // The request was made but no response was received
                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                         // http.ClientRequest in node.js
-                        //console.log("Request error on " + url + ": " + JSON.stringify(error.request));
-                        reject(stringify(error.request));
+                        console.log("Request error on " + url + ": " + JSON.stringify(error.request));
+                        reject(error.request);
                     } 
                     else {
                         // Something happened in setting up the request that triggered an Error
