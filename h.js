c3d17a8c925be1dfe467ad259fdaa33dafd9c61c
Luke Horgan <horgan.l@husky.neu.edu>
2020-01-15 18:13:37 -0500

    grk"

--- h.js
+++ h.js
@@ -332,7 +332,7 @@ class UrlProcessor {
                         // The request was made but no response was received
                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                         // http.ClientRequest in node.js
-                        console.log("Request error on " + url + ": " + JSON.stringify(error.request));
+                        console.log(error.request);
                         reject(error.request);
                     } 
                     else {
