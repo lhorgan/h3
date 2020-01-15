5c272c139db38aa137f14ef48a9440793765c6f5
Luke Horgan <horgan.l@husky.neu.edu>
2020-01-15 18:10:50 -0500

    see the error

--- h.js
+++ h.js
@@ -333,7 +333,7 @@ class UrlProcessor {
                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                         // http.ClientRequest in node.js
                         //console.log("Request error on " + url + ": " + JSON.stringify(error.request));
-                        reject(JSON.stringify(error.request));
+                        reject(stringify(error.request));
                     } 
                     else {
                         // Something happened in setting up the request that triggered an Error
