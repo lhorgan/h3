2d6418ed3bffdda87abcd30e5e50cafb607ac318
Luke Horgan <horgan.l@husky.neu.edu>
2020-01-15 18:02:59 -0500

    grk

--- h.js
+++ h.js
@@ -332,7 +332,7 @@ class UrlProcessor {
                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                         // http.ClientRequest in node.js
                         //console.log("Request error on " + url + ": " + JSON.stringify(error.request));
-                        reject(JSON.stringify(error.request));
+                        reject(error.request);
                     } 
                     else {
                         // Something happened in setting up the request that triggered an Error
